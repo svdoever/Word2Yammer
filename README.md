721df7d8cd9af950127cee84ed098e6b14b83028
Serge van den Oever <svandenoever@deloitte.nl>
2017-05-09 16:21:46 +0200

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# Word2Yammer
-Convert a Microsoft Word document to text to be used as Yammer post
+# About Word2Yammer
+
+Yammer is a communication tool from Microsoft to connect people and teams accross an organization. See https://www.yammer.com/.
+
+Since Yammer is now also able to edit a post, I have become a bit more enthusiastic about Yammer. Unfortunately, the formatting of text in Yammer is still completely absent, and Yammer has the annoying habit of removing spaces at the beginning of a line, so source‑code with indentation becomes unreadable.
+
+When writing larger posts for Yammer it also a nightmare to do the editing in the "Share something with this group..." box.
+
+Microsoft Word and some PowerShell scripting to the rescue!
+
+I created a PowerShell script to convert a Word document to a plain‑text UTF‑8 document with spaces converted to UTF‑8 non‑breaking spaces. If you open the resulting text file into Notepad and copy/paste it into a Yammer post you will get text with all the special characters used in your Word document with the correct indentation for source‑code.
+
+You can now leverage all the power of Word like spell‑checking, text formatting with numbered items, bullet points and indented source‑code and convert it to a readable Yammer document.
+
+# Download
+
+The PowerShell script Word2Yammer.ps1 can be downloaded [here](https://github.com/svdoever/Word2Yammer/blob/master/Word2Yammer.ps1).
+
+# Usage 
+
+Call the script with:
+
+```
+ .\Word2Yammer.ps1 ‑Path MyWordsToTheWorld.docx
+```
+
+which will result in a file ```MyWordsToTheWorld.txt``` which can be opened in Notepad and copied to the Yammer text box.
+
+# Tips
+
+Some editing tips:
+
+1. If you want an empty line between lines, press <ENTER> twice
+2. Copy your source‑code directly from Visual Studio or Visual Studio to your Word document including colored formatting (will be stripped)
+3. Create a folder "YammerPosts"  and add the Word2Yammer.ps1 script to this same folder. You can now always go back to your original Word document to edit a Yammer post for republication (by editing the existing Yammer post!)
+
+Happy Yammering!
---
+++ Word2Yammer.ps1
@@ -0,0 +1,129 @@
+# Word2Yammer.ps1
+# PowerShell script to convert a Word document to Yammer text.
+# © 2017 Serge van den Oever
+# Features:
+# - Word text is exported as UTF-8 so most special characters work
+# - Leading spaces are replaced by non-breaking (UTF-8) spaces, so leading spaces are shown (useful for source-code in a post)
+# - Dashes are replaced by non-breaking (UTF-8) dashes, so no strange line-breaks are introduced
+# - leading and trailing empty lines are removed
+
+#Requires -Version 3.0
+
+param (
+    [Parameter(Mandatory=$true)][string]$Path
+)
+
+function Word2Text {
+    param(
+        [Parameter(Mandatory=$true)][string]$Path,
+        [Parameter(Mandatory=$true)][string]$Destination  
+    )
+    $word = New-Object -ComObject Word.Application
+    $wordDoc  = $word.Documents.Open($Path, $false, $true)
+
+    # Check Version of Word Installed and create UTF-8 (unicode) text file (see https://msdn.microsoft.com/en-us/library/office/ff839952.aspx)
+    $wordVersion = $word.Version
+    If ($wordVersion -eq '16.0' -Or $wordVersion -eq '15.0') {
+        $wordDoc.SaveAs($Destination, 7) 
+        $wordDoc.Close($false)  
+    }
+    ElseIf ($wordVersion -eq '14.0') 
+    {
+        $wordDoc.SaveAs([ref] $Destination,[ref] 7)
+        $wordDoc.Close([ref]$false)
+    }
+
+    # Close Word
+    [gc]::Collect()
+    [gc]::WaitForPendingFinalizers()
+    $word.Quit()
+    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($word)
+
+    # Cleanup
+    Remove-Variable word
+}
+
+function LeadingSpacesTotNonBreakingSpaces
+{
+    param(
+        [string]$TextLine
+    )
+
+    if ($TextLine -match "(^\s+)") {
+        $numSpaces = $matches[1].Length
+        $text = ([string][char]0x00a0)*$numSpaces + $TextLine.Substring($numSpaces)
+        return $text
+    
+    } else {
+        return $TextLine
+    }
+}
+
+# Foreach line:
+# - trim trailing spaces
+# - replace leading spaces by non-breaking spaces
+# - replaces dashes by non-breaking dashes
+# Determine the leading and trailing enpty lines, and write lines inbetween lines to file
+# Append reference to this script to the file
+function YammerizeText {
+    param (
+        [Parameter(Mandatory=$true)][string]$Path 
+    )
+    $lines = Get-Content -Path $Path
+    for ($i=0; $i -lt $lines.Length; $i++) {
+        $s = $lines[$i]
+        $s = $s.TrimEnd();
+        $s = LeadingSpacesTotNonBreakingSpaces -TextLine $s
+        $s = $s.Replace('-', [char]0x2011)
+        $lines[$i] = $s
+    }
+
+    # determine index of first non-empty line
+    $startIndex = 0
+    for ($i=0; $i -lt $lines.Length; $i++) {
+        if ($lines[$i].Length -eq 0) {
+            $startIndex++
+        } else {
+            break
+        }
+    }
+
+    # determine index of last non-empty line
+    $endIndex = $lines.Length-1
+    for ($i=$lines.Length-1; $i -ge 0; $i--) {
+        if ($lines[$i].Length -eq 0) {
+            $endIndex--
+        } else {
+            break
+        }
+    }
+
+    if ($startIndex -gt $endIndex) {
+        $lines = $null
+    } else {
+        $lines = $lines[$startIndex..$endIndex]
+    }
+
+    # add lines about tool
+    $lines += @(
+        "",
+        "==== Composed with https://github.com/svdoever/Word2Yammer ===="
+    )
+
+
+    Out-File -FilePath $Path -InputObject $lines -Encoding "UTF8" 
+}
+
+$ResolvedPath = Resolve-Path -Path $Path
+if ($ResolvedPath -eq $null) { 
+    Write-Error "The specified path '$Path' does not exist."
+    Exit -1
+}
+$Path = $ResolvedPath.Path
+
+$folder = [System.IO.Path]::GetDirectoryName($Path)
+$filename = [System.IO.Path]::GetFileNameWithoutExtension($Path)
+$outfile = "$folder\$filename.txt"
+
+Word2Text -Path $Path -Destination $outfile
+YammerizeText -Path $outfile
